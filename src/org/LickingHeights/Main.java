1802ce8381ed82b60bbd4e49cdf1bed25626c6f8
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-10-24 10:18:26 -0400

    first commit

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,13 @@
+package org.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int number;
+        do {
+
+        }
+
+    }
+}

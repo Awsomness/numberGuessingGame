6eaacf603eeea80937a3ca36a1f32c3334dfd6d0
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-10-28 10:00:51 -0400

    logic loop

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -1,13 +1,46 @@
 package org.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        int number;
-        do {
+        Scanner keyboard;
+        keyboard = new Scanner (System.in);
+        int computerNumber;
+        int guessedNumber;
+        String high,low,gotIt;
+        high = "You're to high";
+        low = "Your to low";
+        computerNumber = (int)(Math.random()*100) + 1;
+        System.out.println("Guess a Number between 1-100");
 
-        }
+
+        do {
+            guessedNumber = keyboard.nextInt();
+            if (guessedNumber > computerNumber){
+                System.out.println(high);
+
+            }
+            else if (guessedNumber < computerNumber){
+                System.out.println(low);
+            }
+            else
+                System.out.println("Corect");
+
+
+        }while (guessedNumber != computerNumber);
+
+
 
     }
+
+
+
+
+
+
+
+
 }
